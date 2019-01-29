6d66c88c21372d0c434e6d70b060dadddcd315d3
Sankalp100 <sankalpgour123@gmail.com>
2019-01-29 09:16:18 +0530

    update

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,21 +1,19 @@
 import React, { Component } from 'react';
-import New from './New';
+import Ninjas from './Ninjas'
 
 class App extends Component {
-  state={
-    news : [
-      { name: "Ryu", age:30, belt: 'black', id:1},
-      {name: "ajju", age:20, belt: 'green', id:2},
-      {name: "vocky", age:25, belt: 'pink', id:3}
+  state = {
+    ninjas: [
+      { name: 'Ryu', age: 30, belt: 'black', id: 1 },
+      { name: 'Yoshi', age: 20, belt: 'green', id: 2 },
+      { name: 'Crystal', age: 25, belt: 'pink', id: 3 }
     ]
   }
   render() {
     return (
       <div className="App">
         <h1>My first React app</h1>
-        <p>Welcome :)</p>
-        <News news = {this.state.news}/>
-        
+        <Ninjas ninjas={this.state.ninjas}/>
       </div>
     );
   }
