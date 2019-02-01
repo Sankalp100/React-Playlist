0b74ab8637f89bebe2f1facfec9e8324de0c9546
Sankalp100 <sankalpgour123@gmail.com>
2019-02-01 14:46:46 +0530

    update

---
+++ React-Redux Complete/todoapp/src/todos.js
@@ -0,0 +1,19 @@
+import React from 'react'       
+
+const Todos = (todos) => {
+
+  const todolist = todo.length ? (
+    todos.map(todo => {
+      return(
+              
+      )
+    })      
+  ) : (
+    <p className="center">You have no todo's left, yay1</p>      
+  )      
+  return(
+    <div className="Todos Collection">
+
+    </div>      
+  )
+}
