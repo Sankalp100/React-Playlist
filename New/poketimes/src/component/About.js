a699d9b17f11a3d8bf05b8f36d5f4bd7021a26aa
Sankalp100 <sankalpgour123@gmail.com>
2019-02-02 17:57:01 +0530

    Poke,time

---
+++ New/poketimes/src/component/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About  = () => {
+    return (
+        <div className="comtainer">
+            <h4 className="center"> About </h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem aspernatur, porro, ipsam nam quos accusantium modi repudiandae provident tempore sequi tempora voluptatum, assumenda minus pariatur. Fuga aliquam rerum ea sunt.</p>
+            
+        </div>
+    )
+}
+
+export default About 
