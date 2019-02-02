9b13f7e520dfefe83230deeb6a8e29f59d593866
Sankalp100 <sankalpgour123@gmail.com>
2019-02-02 18:25:47 +0530

    update

--- New/poketimes/src/App.js
+++ New/poketimes/src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
-import Navbar from './component/Navbar'
+import Navbar from './component/Navbar';
+import { BrowserRouter, Route } from 'react-router-dom'
+import Home from './component/home';
+import About from './component/About';
+import Contact from './component/Contact';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-       <Navbar/>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Route path='/' component={Home} />
+          <Route path='/About' component={About} />
+          <Route path='/Contact' component={Contact} />
+        </div>
+      </BrowserRouter>
     );
   }
 }
